88aca6a00b9d79366bdd3b2922233692c439b370
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2025-02-20 11:01:33 -0300

    Update button styling in TeamFormation component to use centralized app color variables

--- components/TeamFormation.tsx
+++ components/TeamFormation.tsx
@@ -25,7 +25,7 @@ export function TeamFormation() {
         />
       </View>
       <TouchableOpacity
-        className="items-center rounded-lg bg-indigo-600 p-4"
+        className="bg-app-base items-center rounded-lg p-4"
         onPress={handleFormTeams}>
         <Text className="text-base font-bold text-white">Form Teams</Text>
       </TouchableOpacity>
